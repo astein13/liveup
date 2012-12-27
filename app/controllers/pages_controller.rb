4eb5c60887c20668e2ad0ccf4e9a2908052037a8
astein13 <astein13@amherst.edu>
2012-12-27 18:10:15 -0500

    push

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,31 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def donate
+  end
+
+  def partners
+  end
+
+  def participants
+  end
+
+  def programs
+  end
+
+  def scholarship_app
+  end
+
+  def participant_app
+  end
+
+  def mentor_app
+  end
+
+  def about
+  end
+
+  def mentors
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LiveUp::Application.config.secret_token = '9547acf237a7d3e2d237d9fa8bd2c5466391517c0559a4bf276470a707a394a341287f520608bf8c727c3cde5465f875bd086a3cfdb7d7923aebd9c2288d88ed'
